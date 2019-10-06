2351caded7cf9a2f9d8a2bd276c4ba71fceb0f7b
joshtom <olajidejoshua4real@gmail.com>
2019-10-06 01:24:24 +0100

    Inject HttpClient into the constructor of the CartService component class:

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
    providedIn: 'root'
@@ -18,6 +19,8 @@ export class CartService {
     this.items = [];
     return this.items;
   }
-  constructor() { }
+  constructor(
+    
+  ) { }
 
 }
